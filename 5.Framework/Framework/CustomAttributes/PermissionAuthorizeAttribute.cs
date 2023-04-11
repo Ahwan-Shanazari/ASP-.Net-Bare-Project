c452dfcdbf42c397bc946dcf2778d080dee5fa5d
Ahwan-Shanazari <ahwanshanazari2332@gmail.com>
2023-04-11 22:01:42 +0330

    PermissionAuthorizeAttribute updated

--- 5.Framework/Framework/CustomAttributes/PermissionAuthorizeAttribute.cs
+++ 5.Framework/Framework/CustomAttributes/PermissionAuthorizeAttribute.cs
@@ -1,5 +1,6 @@
 using System.Security.Claims;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
 
 namespace Framework.CustomAttributes;
@@ -8,6 +9,15 @@ public class PermissionAuthorizeAttribute :Attribute,IAuthorizationFilter
 {
     public void OnAuthorization(AuthorizationFilterContext context)
     {
-        //logis must be implimented here
+        var userName = context.HttpContext.User.FindFirstValue(ClaimTypes.Name);
+        if (string.IsNullOrEmpty(userName))
+        {
+            context.Result = new StatusCodeResult(401);
+            return;
+        }
+            
+        if (!userName.ToLower().Equals("superadmin"))
+            context.Result = new StatusCodeResult(401);
+
     }
 }
