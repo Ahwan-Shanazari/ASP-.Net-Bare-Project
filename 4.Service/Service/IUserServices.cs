03c8e0b288f2436b94040887f058727f117f2583
Ahwan-Shanazari <ahwanshanazari2332@gmail.com>
2023-04-11 10:48:57 +0330

    UserServices updated & its Interface added

---
+++ 4.Service/Service/IUserServices.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Identity;
+
+namespace Service;
+
+public interface IUserServices
+{
+    Task<bool> CreateAccount(IdentityUser<long> user, string pass);
+    Task DeleteAccount(IdentityUser<long> user);
+    Task<bool> Login(string userName , string pass);
+    Task Logout();
+}
--- 4.Service/Service/UserServices.cs
+++ 4.Service/Service/UserServices.cs
@@ -4,13 +4,45 @@ using Service.Base;
 
 namespace Service;
 
-public class UserServices : BaseServices<IdentityUser<long>>
+public class UserServices : BaseServices<IdentityUser<long>>, IUserServices
 {
     private readonly UserManager<IdentityUser<long>> _userManager;
+    private readonly SignInManager<IdentityUser<long>> _signInManager;
 
-    public UserServices(BaseRepository<IdentityUser<long>> repository, UserManager<IdentityUser<long>> userManager) :
-        base(repository)
+    public UserServices(IBaseRepository<IdentityUser<long>> repository, UserManager<IdentityUser<long>> userManager,
+        IUnitOfWork unitOfWork,SignInManager<IdentityUser<long>> signInManager) :
+        base(repository, unitOfWork)
     {
         _userManager = userManager;
+        _signInManager = signInManager;
+    }
+
+    public async Task<bool> CreateAccount(IdentityUser<long> user, string pass)
+    {
+        var result = await _userManager.CreateAsync(user, pass);
+        
+        if (result.Succeeded)
+            return true;
+        
+        return false;
+    }
+
+    public async Task DeleteAccount(IdentityUser<long> user)
+    {
+        _repository.Delete(user);
+
+        await _unitOfWork.CommitChangesAsync();
+    }
+
+    public async Task<bool> Login(string userName , string pass)
+    {
+        var user = await _repository.Read(user => user.UserName == userName);
+        var result = await _signInManager.PasswordSignInAsync(user, pass , true , false);
+        return result.Succeeded;
+    }
+
+    public async Task Logout()
+    {
+        await _signInManager.SignOutAsync();
     }
 }
