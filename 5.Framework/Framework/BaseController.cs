b07379ab3b39a9ad3d08dde581c5add68a369a7e
Ahwan-Shanazari <ahwanshanazari2332@gmail.com>
2023-04-11 12:01:54 +0330

    BaseController updated

--- 5.Framework/Framework/BaseController.cs
+++ 5.Framework/Framework/BaseController.cs
@@ -1,10 +1,23 @@
+using System.Security.Claims;
+using AutoMapper;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Framework;
 
 [ApiController]
 [Route("api/[controller]/[action]")]
-public class BaseController:ControllerBase
+public abstract class BaseController : ControllerBase
 {
-    
+    protected readonly IMapper _mapper;
+    private readonly HttpContext _context;
+
+    public BaseController(IMapper mapper, HttpContext context)
+    {
+        _mapper = mapper;
+        _context = context;
+    }
+
+    protected string CurrentUserId => _context.User?.FindFirstValue(ClaimTypes.NameIdentifier) ?? "0";
+    protected string CurrentUserName => _context.User?.FindFirstValue(ClaimTypes.Name) ?? "";
 }
