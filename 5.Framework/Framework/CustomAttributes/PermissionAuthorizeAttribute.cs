5648fa5c83a77b7f701d912f90aa7feb1929e24a
BLACK3X <ahwanshanazari2332@gmail.com>
2023-04-18 19:21:27 +0430

    Permission Authorize Attribute updated

--- 5.Framework/Framework/CustomAttributes/PermissionAuthorizeAttribute.cs
+++ 5.Framework/Framework/CustomAttributes/PermissionAuthorizeAttribute.cs
@@ -12,28 +12,50 @@ public class PermissionAuthorizeAttribute : Attribute, IAuthorizationFilter
 {
     private readonly IUserClaimsRepository _userClaimsRepository;
     private readonly IUserRolesRepository _userRolesRepository;
+    private readonly IRoleClaimRepository _roleClaimRepository;
 
-    public PermissionAuthorizeAttribute(IUserClaimsRepository userClaimsRepository,IUserRolesRepository userRolesRepository)
+    public PermissionAuthorizeAttribute(IUserClaimsRepository userClaimsRepository,IUserRolesRepository userRolesRepository,IRoleClaimRepository roleClaimRepository)
     {
         _userClaimsRepository = userClaimsRepository;
         _userRolesRepository = userRolesRepository;
+        _roleClaimRepository = roleClaimRepository;
     }
     public void OnAuthorization(AuthorizationFilterContext context)
     {
         var userName = context.HttpContext.User.FindFirstValue(ClaimTypes.Name);
-        var userId = long.Parse(context.HttpContext.User.FindFirstValue(ClaimTypes.NameIdentifier));
-        if (string.IsNullOrEmpty(userName))
+        var userIdStr = context.HttpContext.User.FindFirstValue(ClaimTypes.NameIdentifier);
+        
+        if (string.IsNullOrEmpty(userName) || string.IsNullOrEmpty(userIdStr))
+        {
             context.Result = new StatusCodeResult(401);
+            return;
+        }
 
-        else if (!userName.ToLower().Equals("superadmin"))
-            if(!CheckAuthorization(context,userId))
-                context.Result = new StatusCodeResult(401);
+
+        if (userName.ToLower().Equals("superadmin"))
+            return;
+        
+        var userId = long.Parse(userIdStr);
+        
+        if(!CheckAuthorization(context,userId))
+            context.Result = new StatusCodeResult(401);
     }
 
     private bool CheckAuthorization(AuthorizationFilterContext context,long userId)
     {
-        var userClaims = _userClaimsRepository.GetUserPermissions(userId);
-        var userRoleId = _userRolesRepository.GetUserRoleIds(userId);
-        return true;
+        var route = context.HttpContext.Request.Path.Value;
+        var userPermissions = _userClaimsRepository.GetUserPermissions(userId).Select(claim => claim.Value);
+        if (userPermissions.Contains(route,StringComparer.OrdinalIgnoreCase))
+        {
+            return true;
+        }
+        var userRoleIds = _userRolesRepository.GetUserRoleIds(userId);
+        foreach (var userRoleId in userRoleIds)
+        {
+            if (_roleClaimRepository.GetRoleClaims(userRoleId).Select(claim => claim.Value)
+                .Contains(route, StringComparer.OrdinalIgnoreCase))
+                return true;
+        }
+        return false;
     }
 }
