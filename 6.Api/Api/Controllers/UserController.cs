ce679240f9a84e08f7060a130901bfc4fe3ed26a
BLACK3X <ahwanshanazari2332@gmail.com>
2023-04-23 20:55:06 +0430

    login action changed

--- 6.Api/Api/Controllers/UserController.cs
+++ 6.Api/Api/Controllers/UserController.cs
@@ -1,9 +1,12 @@
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
 using Api.Dtos;
 using AutoMapper;
 using Framework;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
 using Service;
 using Service.Interfaces;
 
@@ -12,20 +15,29 @@ namespace Api.Controllers;
 public class UserController : BaseController
 {
     private readonly IUserServices _userServices;
+    private readonly IConfiguration _configuration;
 
-    public UserController(IUserServices userServices, IMapper mapper) 
+    public UserController(IUserServices userServices, IMapper mapper,IConfiguration configuration) 
         : base(mapper)
     {
         _userServices = userServices;
+        _configuration = configuration;
     }
 
     [HttpPost]
     public async Task<IActionResult> Login(UserLoginDto user)
     {
-        var result = await _userServices.Login(user.UserName, user.Password);
-        if (result)
-            return Ok();
-        return BadRequest();
+        // var result = await _userServices.Login(user.UserName, user.Password);
+        // if (result)
+        //     return Ok();
+        // return BadRequest();
+        
+        //ToDo: replace this with custom appsettings class
+        var key = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(_configuration["JwtOptions"]));
+        var signInCridentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+        //var tokenOptions = new JwtSecurityToken()
+        
+        return Ok();
     }
 
     [HttpPost]
