215c511878d116eda3a36af6c69ec474d51dc033
Ahwan-Shanazari <ahwanshanazari2332@gmail.com>
2023-04-24 12:18:32 +0330

    A ToDo Comment Added

--- 3.Data/Data/Contexts/DataContext.cs
+++ 3.Data/Data/Contexts/DataContext.cs
@@ -4,6 +4,7 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Data.Contexts;
 
+//ToDo: Use Custom User And Roles And Use Custom Classes for Other Need Generic Types in context
 public class DataContext : IdentityDbContext<IdentityUser<long>,IdentityRole<long>,long>
 {
     public DataContext(DbContextOptions<DataContext> options):base(options)
