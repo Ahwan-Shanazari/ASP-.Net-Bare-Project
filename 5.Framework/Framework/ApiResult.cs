e91bc416e816e3643f7273277e599c73b3ea85db
Ahwan-Shanazari <ahwanshanazari2332@gmail.com>
2023-04-24 10:56:37 +0330

    Custom Api result created

---
+++ 5.Framework/Framework/ApiResult.cs
@@ -0,0 +1,30 @@
+using System.Net;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Framework;
+
+public class ApiResult
+{
+    protected ApiResult Self;
+    
+    public ApiResult()
+    {
+        Self = this;
+    }
+    public bool IsSuccess { get; set; }
+    public string? Message { get; set; }
+    public HttpStatusCode StatusCode { get; set; }
+
+    public  static implicit operator ActionResult(ApiResult result)
+    {
+        return new ObjectResult(result.Self)
+        {
+            StatusCode = (int)result.StatusCode
+        };
+    }
+}
+
+public class ApiResult<T>:ApiResult where T: class
+{
+    public T Data { get; set; }
+}
