f3de002340433ac7612e1b50937b1f3a0241a14f
Ahwan-Shanazari <ahwanshanazari2332@gmail.com>
2023-04-25 13:39:36 +0330

    Program.cs updated

--- 6.Api/Api/Program.cs
+++ 6.Api/Api/Program.cs
@@ -1,16 +1,12 @@
 using System.Reflection;
-using System.Text;
 using Data.Contexts;
 using Data.Repositories;
 using Data.Repositories.Base;
 using Framework;
+using Framework.Configurations;
+using Framework.Configurations.Initializers;
 using Framework.CustomAttributes;
-using Framework.Initializers;
 using Framework.Interfaces;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.IdentityModel.Tokens;
 using Service;
 using Service.Interfaces;
 
@@ -19,8 +15,11 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-builder.Services.AddSingleton<IRouteDetector,RouteDetector>();
+
+//adding swagger with requirements
+builder.Services.AddSwaggerWithJwtSupport();
+
+builder.Services.AddSingleton<IRouteDetector, RouteDetector>();
 builder.Services.AddAutoMapper(Assembly.GetExecutingAssembly());
 builder.Services.AddScoped<PermissionAuthorizeAttribute>();
 //ToDo: Inject the repositories dynamically using reflection and an assembly of data layer. look for their Interfaces and inject them with that. 
@@ -33,40 +32,14 @@ builder.Services.AddScoped<IUserRolesRepository, UserRolesRepository>();
 builder.Services.AddScoped<IRoleClaimRepository, RoleClaimRepository>();
 
 // Injecting The Context
-builder.Services.AddDbContext<DataContext>(optionsBuilder =>
-    optionsBuilder.UseSqlServer(builder.Configuration.GetConnectionString("PermissionAuthDb")));
-builder.Services.AddIdentity<IdentityUser<long>, IdentityRole<long>>().AddEntityFrameworkStores<DataContext>();
-builder.Services.Configure<IdentityOptions>(options =>
-{
-    options.Password.RequireDigit = false;
-    options.Password.RequiredLength = 4;
-    options.Password.RequireLowercase = false;
-    options.Password.RequireUppercase = false;
-    options.Password.RequireNonAlphanumeric = false;
-});
+builder.Services.AddDefaultIdentityForAContext<DataContext>(builder.Configuration,"PermissionAuthDb");
 
 //adding authentication and jwt services
-builder.Services.AddAuthentication(opt => {
-        opt.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-        opt.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-    })
-    .AddJwtBearer(options =>
-    {
-        options.TokenValidationParameters = new TokenValidationParameters
-        {
-            ValidateIssuer = true,
-            ValidateAudience = true,
-            ValidateLifetime = true,
-            ValidateIssuerSigningKey = true,
-            ValidIssuer = builder.Configuration["JwtOptions:ValidIssuer"],
-            ValidAudience = builder.Configuration["JwtOptions:ValidAudience"],
-            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["JwtOptions:Key"]))
-        };
-    });
+builder.Services.AddJwtServices();
 
 var app = builder.Build();
 
-await app.InitialDbWithSuperAdmin(app.Services);
+await app.InitialDbWithSuperAdmin(app.Services.CreateScope().ServiceProvider);
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
