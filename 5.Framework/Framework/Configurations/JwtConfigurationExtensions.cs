38483fcddae723a30b69c365eb554203edbf8161
Ahwan-Shanazari <ahwanshanazari2332@gmail.com>
2023-05-04 16:02:26 +0330

    other jwt events added

--- 5.Framework/Framework/Configurations/JwtConfigurationExtensions.cs
+++ 5.Framework/Framework/Configurations/JwtConfigurationExtensions.cs
@@ -1,6 +1,7 @@
 using System.Text;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -10,10 +11,24 @@ namespace Framework.Configurations;
 
 public static class JwtConfigurationExtensions
 {
+    private static string FlattenException(Exception exception)
+    {
+        var stringBuilder = new StringBuilder();
+
+        while (exception != null)
+        {
+            stringBuilder.AppendLine(exception.Message);
+            stringBuilder.AppendLine(exception.StackTrace);
+
+            exception = exception.InnerException;
+        }
+
+        return stringBuilder.ToString();
+    }
+
     public static AuthenticationBuilder AddJwtServices(this IServiceCollection services)
     {
-        
-        
+        string message = "";
         return services.AddAuthentication(opt =>
             {
                 opt.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
@@ -32,16 +47,44 @@ public static class JwtConfigurationExtensions
                     ValidateIssuerSigningKey = true,
                     ValidIssuer = configuration["JwtOptions:ValidIssuer"],
                     ValidAudience = configuration["JwtOptions:ValidAudience"],
-                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["JwtOptions:Key"])),
-                    TokenDecryptionKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["JwtOptions:KeyX"])),
+                    IssuerSigningKey =
+                        new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["JwtOptions:Key"])),
+                    TokenDecryptionKey =
+                        new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["JwtOptions:KeyX"])),
                     ClockSkew = TimeSpan.Zero
                 };
                 options.Events = new JwtBearerEvents
                 {
+                    OnAuthenticationFailed = ctx =>
+                    {
+                        ctx.Response.StatusCode = StatusCodes.Status302Found;
+                        message += "From OnAuthenticationFailed:\n";
+                        message += FlattenException(ctx.Exception);
+                        return Task.CompletedTask;
+                    },
+
+                    OnChallenge = ctx =>
+                    {
+                        message += "From OnChallenge:\n";
+                        ctx.Response.StatusCode = StatusCodes.Status401Unauthorized;
+                        ctx.Response.ContentType = "text/plain";
+                        return ctx.Response.WriteAsync(message);
+                    },
+
+                    OnMessageReceived = ctx =>
+                    {
+                        message = "From OnMessageReceived:\n";
+                        ctx.Request.Headers.TryGetValue("Authorization", out var BearerToken);
+                        if (BearerToken.Count == 0)
+                            BearerToken = "no Bearer token sent\n";
+                        message += "Authorization Header sent: " + BearerToken + "\n";
+                        return Task.CompletedTask;
+                    },
                     OnTokenValidated = async context =>
                     {
                         var claims = context.Principal.Claims;
-                        var userName = claims.FirstOrDefault(claim => claim.Type == "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/name");
+                        var userName = claims.FirstOrDefault(claim =>
+                            claim.Type == "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/name");
 
                         var userManger = services.BuildServiceProvider().GetService<UserManager<IdentityUser<long>>>();
                         var user = await userManger.FindByNameAsync(userName.Value);
@@ -49,9 +92,8 @@ public static class JwtConfigurationExtensions
                         var stampClaim = claims.FirstOrDefault(claim => claim.Type == "SecurityStamp");
                         if (stampClaim is null || stampClaim.Value != user.SecurityStamp)
                             context.Fail("Security Stamp Is Expired");
-            
                     }
                 };
             });
-    } 
+    }
 }
