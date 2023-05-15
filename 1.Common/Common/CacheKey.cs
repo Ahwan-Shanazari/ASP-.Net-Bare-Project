26a0aa54f646860e764430effdaffa13611889e3
Ahwan-Shanazari <ahwanshanazari2332@gmail.com>
2023-05-15 12:21:28 +0330

    Cache removing methods added to UOW class

---
+++ 1.Common/Common/CacheKey.cs
@@ -0,0 +1,9 @@
+namespace Common;
+
+public enum CacheKey
+{
+    IdentityUser,
+    IdentityUserRole,
+    IdentityUserClaim,
+    IdentityRoleClaim
+}
--- 3.Data/Data/Repositories/Base/UnitOfWork.cs
+++ 3.Data/Data/Repositories/Base/UnitOfWork.cs
@@ -1,18 +1,28 @@
+using Common;
 using Data.Contexts;
+using Microsoft.Extensions.Caching.Memory;
 
 namespace Data.Repositories.Base;
 
 public class UnitOfWork : IUnitOfWork
 {
     private readonly DataContext _context;
+    private readonly IMemoryCache _cache;
 
-    public UnitOfWork(DataContext context)
+    public UnitOfWork(DataContext context, IMemoryCache cache)
     {
         _context = context;
+        _cache = cache;
     }
 
     public int CommitChanges()
     {
+        return _context.SaveChanges();
+    }
+    
+    public int CommitChanges(CacheKey key)
+    {
+        _cache.Remove(key.ToString());
         return _context.SaveChanges();
     }
 
@@ -20,4 +30,11 @@ public class UnitOfWork : IUnitOfWork
     {
         return await _context.SaveChangesAsync();
     }
+    
+    public async Task<int> CommitChangesAsync(CacheKey key)
+    {
+        _cache.Remove(key.ToString());
+        return await _context.SaveChangesAsync();
+    }
+    
 }
